4ed0b9321952970f9432e4b4011c125e8ff7db74
Jani Miettinen <Miettinenjani1@gmail.com>
2024-07-22 20:43:24 +0300

    Feature: Delete Bowler

---
+++ src/Application/Bowlers/Commands/CreateBowler/DeleteBowler.cs
@@ -0,0 +1,25 @@
+﻿using BowlingApp.Application.Common.Interfaces;
+using BowlingApp.Application.Common.Services;
+
+namespace BowlingApp.Application.Bowlers.Commands.CreateBowler;
+
+public record DeleteBowlerCommand(string Id) : IRequest;
+
+public class DeleteBowlerCommandHandler(IApplicationDbContext context, StatisticService statisticService)
+    : IRequestHandler<DeleteBowlerCommand>
+{
+    private readonly IApplicationDbContext _context = context;
+    private readonly StatisticService _statisticService = statisticService;
+
+    public async Task Handle(DeleteBowlerCommand request, CancellationToken cancellationToken)
+    {
+        var bowler = await _context.Bowlers.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+        Guard.Against.NotFound(request.Id, bowler);
+
+        _context.Bowlers.Remove(bowler);
+        await _context.SaveChangesAsync(cancellationToken);
+
+        await _statisticService.RecalculateCalculatedValues(cancellationToken);
+    }
+}
--- src/Web/Endpoints/Bowlers.cs
+++ src/Web/Endpoints/Bowlers.cs
@@ -10,7 +10,7 @@ public class Bowlers : EndpointGroupBase
     public override void Map(WebApplication app)
     {
         app.MapGroup(this).MapGet(GetBowlers).MapGet(GetBowlerReport, "Report");
-        app.MapGroup(this).RequireAuthorization().MapPost(CreateBowler);
+        app.MapGroup(this).RequireAuthorization().MapPost(CreateBowler).MapDelete(DeleteBowler, "id");
     }
 
     public async Task<WeeklyResultsDto> GetMostRecentWeekResults(ISender sender)
@@ -26,6 +26,12 @@ public class Bowlers : EndpointGroupBase
     public async Task<BowlerDto> CreateBowler(ISender sender, CreateBowlerCommand command)
     {
         return await sender.Send(command);
+    }
+
+    public async Task<IResult> DeleteBowler(ISender sender, string id)
+    {
+        await sender.Send(new DeleteBowlerCommand(id));
+        return Results.NoContent();
     }
 
     public async Task<BowlerReport> GetBowlerReport(ISender sender, string bowlerId, string? seasonId)
