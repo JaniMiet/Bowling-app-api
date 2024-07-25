11e7b5534d3422122c7ef725d2584ff8acb592d1
Jani Miettinen <Miettinenjani1@gmail.com>
2024-07-25 17:29:37 +0300

    Removed Hsts

--- src/Web/Program.cs
+++ src/Web/Program.cs
@@ -29,11 +29,6 @@ if (app.Environment.IsDevelopment())
 {
     await app.InitialiseDatabaseAsync();
 }
-else
-{
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
 app.UseHealthChecks("/health");
 
