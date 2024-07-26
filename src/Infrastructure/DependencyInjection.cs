5ca11b297e420be6f001b3d3a6c3a1b2e1f33a78
Jani Miettinen <Miettinenjani1@gmail.com>
2024-07-26 11:29:25 +0300

    Test

--- src/Infrastructure/DependencyInjection.cs
+++ src/Infrastructure/DependencyInjection.cs
@@ -20,7 +20,6 @@ public static class DependencyInjection
     )
     {
         var connectionString = configuration.GetConnectionString("DefaultConnection");
-        var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
 
         Guard.Against.Null(connectionString, message: "Connection string 'DefaultConnection' not found.");
 
@@ -61,7 +60,7 @@ public static class DependencyInjection
             {
                 options.Authority = configuration.GetValue<string>("Auth:Authority");
                 options.Audience = configuration.GetValue<string>("Auth:Audience");
-                options.RequireHttpsMetadata = environment != "Development";
+                options.RequireHttpsMetadata = false;
                 options.TokenValidationParameters = new TokenValidationParameters()
                 {
                     NameClaimType = ClaimTypes.NameIdentifier
